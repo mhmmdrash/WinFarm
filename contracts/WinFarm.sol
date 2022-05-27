013b3a1269c7b74f5d13f545c982afd60f3a85c0
Muhammed rasheed <103559722+mhmmdrash@users.noreply.github.com>
2022-05-27 14:23:21 +0530

    Update WinFarm.sol

--- contracts/WinFarm.sol
+++ contracts/WinFarm.sol
@@ -1,107 +1,68 @@
-// SPDX-License-Identifier: MIT
+// SPDX=License-Identifier: MIT
 pragma solidity ^0.8;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./WinToken.sol";
 import "./RenToken.sol";
 
-contract WinFarm {
-    // userAddress => stakingBalance
-    mapping(address => uint256) public stakingBalance;
-    // userAddress => isStaking boolean
-    mapping(address => bool) public isStaking;
-    // userAddress => timeStamp
-    mapping(address => uint256) public startTime;
-    // userAddress => pmknBalance
-    mapping(address => uint256) public winBalance;
+contract WinFarm2 {
 
-    string public name = "WinFarm";
+    struct staker {
+        uint balance;
+        bool isStaking;
+        uint startTime;
+        uint storedRewards;
+    }
+    mapping(address => staker) public stakers;
+    RenToken public immutable renToken;
+    WinToken public immutable winToken;
 
-    RenToken public renToken;
-    WinToken public winToken;
-
-    event Stake(address indexed from, uint256 amount);
-    event Unstake(address indexed from, uint256 amount);
-    event YieldWithdraw(address indexed to, uint256 amount);
-
-    constructor(RenToken _renToken, WinToken _winToken) public {
+    constructor(RenToken _renToken, WinToken _winToken) {
         renToken = _renToken;
         winToken = _winToken;
     }
 
-
-    /// Core function shells
-
-    function stake(uint amt) public {
-        require(renToken.balanceOf(msg.sender) >= amt && amt > 0, "Cannot stake zero tokens");
-        if(isStaking[msg.sender] == true){
-            uint256 toTransfer = calculateYieldTotal(msg.sender);
-            winBalance[msg.sender] += toTransfer;
+    function stake(uint amount) public {
+        require(renToken.balanceOf(msg.sender) >= amount, "Insufficient balance");
+        if(stakers[msg.sender].isStaking) {
+            uint rew = calculateReward(msg.sender);
+            stakers[msg.sender].storedRewards += rew;
         }
-        renToken.transferFrom(msg.sender, address(this), amt);
-        stakingBalance[msg.sender] += amt;
-        startTime[msg.sender] = block.timestamp;
-        isStaking[msg.sender] = true;
-        emit Stake(msg.sender, amt);
+        renToken.transferFrom(msg.sender, address(this), amount);
+        stakers[msg.sender].balance += amount;
+        stakers[msg.sender].isStaking = true;
+        stakers[msg.sender].startTime = block.timestamp;
     }
 
-
-    function unstake(uint256 amount) public {
-        require(
-            isStaking[msg.sender] = true &&
-            stakingBalance[msg.sender] >= amount, 
-            "Nothing to unstake"
-        );
-        uint256 yieldTransfer = calculateYieldTotal(msg.sender);
-        startTime[msg.sender] = block.timestamp; // bug fix
-        // uint256 balanceTransfer = amount;
-        // amount = 0;
-        stakingBalance[msg.sender] -= amount;
-        renToken.transfer(msg.sender, amount);
-        winBalance[msg.sender] += yieldTransfer;
-        if(stakingBalance[msg.sender] == 0){
-            isStaking[msg.sender] = false;
-        }
-        emit Unstake(msg.sender, amount);
+    function unstake(uint amount) public {
+        require(stakers[msg.sender].balance >= amount && stakers[msg.sender].isStaking, "Nothing to unstake");
+        uint rew = calculateReward(msg.sender);
+        stakers[msg.sender].storedRewards += rew;
+        stakers[msg.sender].balance -= amount;
+        stakers[msg.sender].startTime = block.timestamp;
+        renToken.transferFrom(address(this), msg.sender, amount);
+        if(stakers[msg.sender].balance == 0) stakers[msg.sender].isStaking = false;
     }
 
-
-    function viewYieldBalance() public view returns(uint) {
-        uint bal = calculateYieldTotal(msg.sender);
-        bal += winBalance[msg.sender];
-        return bal;
+    function claimRewards() public {
+        uint rew = calculateReward(msg.sender);
+        require(rew > 0 || stakers[msg.sender].storedRewards > 0, "NO Rewards to claim!");
+        uint bal = stakers[msg.sender].storedRewards;
+        stakers[msg.sender].storedRewards = 0;
+        rew += bal;
+        winToken.mint(msg.sender, rew);
     }
-
-
-    function withdrawYield() public {
-        uint toTransfer = calculateYieldTotal(msg.sender);
-        require(
-            toTransfer > 0 || 
-            winBalance[msg.sender] > 0, 
-            "Nothing to withdraw"
-        );
-        if (winBalance[msg.sender] != 0) {
-            uint oldBal = winBalance[msg.sender];
-            winBalance[msg.sender] = 0;
-            toTransfer += oldBal;
-        }
-        startTime[msg.sender] = block.timestamp;
-        winToken.mint(msg.sender, toTransfer);
-        emit YieldWithdraw(msg.sender, toTransfer);
-    }
-
 
     function calculateYieldTime(address user) public view returns (uint) {
-        uint timeDif = block.timestamp - startTime[user];
+        uint timeDif = block.timestamp - stakers[user].startTime;
         return timeDif;
     }
 
-
-    function calculateYieldTotal(address user) public view returns (uint) {
+    // rewards will be distributed 5% quarterly
+    function calculateReward(address user) internal view returns(uint reward) {
         uint time = calculateYieldTime(user) * 10 ** 18;
-        uint rate = 86400;
-        uint timeRate = time / rate;
-        uint rawYield = (stakingBalance[user] * timeRate) / 10 ** 18;
-        return rawYield;
+        uint rate = 86400 * 30 * 3;
+        uint timerate = time / rate;
+        uint yield = stakers[user].balance * 5 / 100;
+        reward = (yield * timerate) / 10 ** 18;
     }
 }
